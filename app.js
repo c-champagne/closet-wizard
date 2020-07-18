dd16fe35dad687e40313fef6aa092c0ab818d969
c-champagne <cassielchampagne@gmail.com>
2020-07-18 09:04:47 -0400

    More progress on file upload

--- app.js
+++ app.js
@@ -65,6 +65,34 @@ passport.use(new GoogleStrategy({
    });
 //End Passport
 const client = require('filestack-js').init(process.env.FILESTACK_APIKEY);
+/* const options = {
+    maxFiles: 5,
+    uploadInBackground: false,
+    onOpen: () => console.log("opened!"),
+    onUploadDone: (res) => console.log (res.filesUploaded[0].handle),
+    onFileUploadFailed: () => console.log('failed'),}
+const picker = client.picker(options); */
+
+/* const form = document.getElementById('pick-form');
+const fileInput = document.getElementById('fileupload');
+const btn = document.getElementById('picker');
+const nameBox = document.getElementById('nameBox');
+const urlBox = document.getElementById('urlBox');
+
+btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    picker.open();
+})
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Submitting' + fileInput.value);
+});
+
+function updateForm (result) {
+    const fileData = result.filesUploaded[0];
+    fileInput.value = fileData.url;
+} */
 
 
 
@@ -118,6 +146,13 @@ app.post('/submitUser', function (req, res) {
         {firstName: name},
         {where: {id: req.user.id}})
         .then(() => res.redirect("/closet"))
+})
+
+app.post('/submitImage', function (req, res) {
+    console.log('submitted')
+    let clothingName = (req.body.clothingName)
+    let itemHandle = (req.body.fsHandle)
+    console.log("Item handle is " + itemHandle)
 })
 
 app.get('/closet/clothes', function (req, res) {
