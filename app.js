e9c1c5155e813d733dbd358e97d134f9f3a98545
c-champagne <cassielchampagne@gmail.com>
2020-07-21 22:28:27 -0400

    Small update

--- app.js
+++ app.js
@@ -269,27 +269,6 @@ app.get('/closet/clothes', function (req, res) {
 }) */
 
 app.get('/closet/outfits', function (req, res) {
-    /* db.outfit.findAll({
-        include: [{
-            model: db.clothing,
-            attributes: ['name', 'image'],
-            where: {user_id: [req.user.id]}
-        }]
-    })
-   .then((results) => {
-       for(i=0; i < results.length; i++) {
-           results[i].clothings.forEach(element => renderImages.push(element.image))
-       }
-   console.log("What is this", renderImages)
-})
-   res.render('outfits', {
-       name: req.user.firstName,
-       clothing: userClothes,
-       outfits: renderImages
-   })  */
-   
-
-
      db.clothing.findAll({
         where: {user_id: [req.user.id]}
     })
@@ -304,13 +283,15 @@ app.get('/closet/outfits', function (req, res) {
              }]
          })
         .then((results) => {
+            let userOutfits = results;
             for(i=0; i < results.length; i++) {
                 results[i].clothings.forEach(element => renderImages.push(element.image))
             }
             res.render('outfits', {
                 name: req.user.firstName,
                 clothing: userClothes,
-                outfits: renderImages
+                outfits: userOutfits,
+                outfitImgs: renderImages
             }) 
         console.log("What is this", renderImages)
         console.log("CO", results[0].clothings[0])
